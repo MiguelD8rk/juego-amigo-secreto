5e8a7c4c577334ac6c79a3d44fe97bcd2f3a4327
Miguel Ángel de la Cruz Lázaro <mdelacruzlazaro269@gmail.com>
2025-08-13 10:33:01 -0600

    Añadida la primera función: agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de los amigos
+let amigos = [];
+
+//Función para agregar un amigo a la lista
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value.trim(); 
+
+    // Validar si el campo de entrada está vacío
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+    } else if (amigos.includes(nombre)) {
+        alert('Ese nombre ya está en la lista.');
+    } else {
+        // Agregar el nombre al array 'amigos'
+        amigos.push(nombre);
+        inputAmigo.value = '';  // Limpiar el campo de texto
+        actualizarLista();  // Actualizar la lista visual
+    }
+}
